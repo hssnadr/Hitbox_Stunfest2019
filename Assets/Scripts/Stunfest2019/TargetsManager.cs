51167837ebc1db03f7dd7d89469d5780a922da15
Adrien Husson <husson.adr@gmail.com>
2019-05-12 11:01:26 +0200

    RGBtoCMJ_targets_behavior

--- Assets/Scripts/Stunfest2019/TargetsManager.cs
+++ Assets/Scripts/Stunfest2019/TargetsManager.cs
@@ -13,10 +13,14 @@ namespace CRI.HitBoxTemplate.Example
         public GameObject targetPrefab;
         List<GameObject> targetsList = new List<GameObject>();
 
-        public GameObject impactPrefab;
+        public GameObject impactPrefab; // prefab to show where the impacts are detected
+        private int delayOffHit = 100 ;
+        private long timerOffHit0 = 0;
 
         private Camera _hitboxCamera;
         public Camera _debugCamera;
+
+        Color[] colors ;
 
         private void OnEnable()
         {
@@ -32,6 +36,14 @@ namespace CRI.HitBoxTemplate.Example
         {
             _hitboxCamera = this.gameObject.GetComponent<Camera>();
             targetsList = new List<GameObject>();
+            colors = new Color[7];
+            colors[0] = Color.red;
+            colors[1] = Color.green;
+            colors[2] = Color.blue;
+            colors[3] = Color.yellow;
+            colors[4] = Color.cyan;
+            colors[5] = Color.magenta;
+            colors[6] = Color.white;
         }
 
         private void OnImpact(object sender, ImpactPointControlEventArgs e)
@@ -47,7 +59,7 @@ namespace CRI.HitBoxTemplate.Example
             Debug.DrawRay(position2D_, cameraForward * 10000, Color.yellow, 10.0f);
 
             bool isTrigger_ = false ;
-            Color targetColor_;
+            Color targetColor_ = Color.black;
             if (targetsList.Count > 0)
             {
                 RaycastHit hit;
@@ -66,21 +78,63 @@ namespace CRI.HitBoxTemplate.Example
                 isTrigger_ = true;
             }
 
+            Debug.Log(targetColor_.ToString());
+
             if (isTrigger_)
-                setCrownTargets(position3D_, 3);
+            {
+                Color[] colTargets_;
+
+                if (targetsList.Count == 0)
+                {
+                    colTargets_ = new Color[3];
+                    colTargets_[0] = Color.red;
+                    colTargets_[1] = Color.green;
+                    colTargets_[2] = Color.blue;
+                    setCrownTargets(position3D_, colTargets_, 50.0f);
+                }
+                else
+                {
+                    if (targetColor_ == Color.red)
+                    {
+                        colTargets_ = new Color[2];
+                        colTargets_[0] = Color.magenta;
+                        colTargets_[1] = Color.yellow;
+                        setCrownTargets(position3D_, colTargets_, 75.0f);
+                    }
+
+                    if (targetColor_ == Color.green)
+                    {
+                        colTargets_ = new Color[2];
+                        colTargets_[0] = Color.yellow;
+                        colTargets_[1] = Color.cyan;
+                        setCrownTargets(position3D_, colTargets_, 75.0f);
+                    }
+
+                    if (targetColor_ == Color.blue)
+                    {
+                        colTargets_ = new Color[2];
+                        colTargets_[0] = Color.cyan;
+                        colTargets_[1] = Color.magenta;
+                        setCrownTargets(position3D_, colTargets_, 75.0f);
+                    }
+                }
+            }
 
             impactPrefab.transform.position = position3D_;
         }
 
-        private void setCrownTargets(Vector3 position_, int nTargets_) {
+        private void setCrownTargets(Vector3 position_, Color[] colTargets_, float speed_)
+        {
             int angle0_ = Random.Range(0, 360);
+            int nTargets_ = colTargets_.Length;
 
-            for (int i = 0; i < nTargets_; i++)
+            for (int i = 0; i < nTargets_ ; i++)
             {
                 targetsList.Add((GameObject)Instantiate(targetPrefab, position_, Quaternion.identity));
-                GameObject target_ = targetsList[targetsList.Count-1];
+                GameObject target_ = targetsList[targetsList.Count - 1];
                 target_.GetComponent<TargetBehavior>().SetAngleDirection(angle0_ + i * (float)360 / nTargets_);
-                target_.GetComponent<TargetBehavior>().SetColor(Color.green);
+                target_.GetComponent<TargetBehavior>().SetColor(colTargets_[i]);
+                target_.GetComponent<TargetBehavior>().SetTranslationSpeed(speed_);
             }
         }
 
