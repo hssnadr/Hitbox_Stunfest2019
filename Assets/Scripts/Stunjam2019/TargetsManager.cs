31e349270988b8b1308966a8153974a69978655d
Adrien Husson <husson.adr@gmail.com>
2020-01-14 15:45:06 +0100

    builFix

--- Assets/Scripts/Stunjam2019/TargetsManager.cs
+++ Assets/Scripts/Stunjam2019/TargetsManager.cs
@@ -325,8 +325,10 @@ namespace CRI.HitBoxTemplate.Example
 
         private void Update()
         {
+#if UNITY_EDITOR
             if (Input.GetMouseButtonDown(0))
                 OnMouseDown();
+#endif
 
             for (int i = 0; i < targetsList.Count; i++) {
                 if (targetsList[i] == null)
